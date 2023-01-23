162ea835627dea2a7faa65200bfff699c9470ca4
Aliaksei Makas <a.d.makas@gmail.com>
2023-01-23 22:42:52 +0400

    feat: add constants

---
+++ src/ws_server/constants.ts
@@ -0,0 +1,21 @@
+export const MOUSE_UP = "mouse_up";
+export const MOUSE_DOWN = "mouse_down";
+export const MOUSE_LEFT = "mouse_left";
+export const MOUSE_RIGHT = "mouse_right";
+export const MOUSE_POSITION = "mouse_position";
+export const DRAW_CIRCLE = "draw_circle";
+export const DRAW_RECTANGLE = "draw_rectangle";
+export const DRAW_SQUARE = "draw_square";
+export const PRNT_SCRN = "prnt_scrn";
+export const STEPS = 500;
+
+export const COMMANDS = [
+  MOUSE_UP,
+  MOUSE_DOWN,
+  MOUSE_LEFT,
+  MOUSE_RIGHT,
+  MOUSE_POSITION,
+  DRAW_CIRCLE,
+  DRAW_SQUARE,
+  DRAW_RECTANGLE,
+];
